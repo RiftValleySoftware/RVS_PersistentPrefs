81188ef5ef60726c374c6ff0d094dd94d60c3e1c
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-20 20:35:39 -0400

    Added supported platforms.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "RVS_Persistent_Prefs",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v11),
+        .macOS(.v10.14),
+        .watchOS(.v5)
+    ],
     products: [
         .library(
             name: "RVS-Persistent-Prefs",
