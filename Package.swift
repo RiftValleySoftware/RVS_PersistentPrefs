95941095c15ca829ab0bbbbe539aeebe64d2fb1f
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-24 08:24:03 -0400

    I now have the dependencies working (release version-based).

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git",
-            from: "1.2.0"
+            from: "1.2.1"
         )
     ],
     targets: [
